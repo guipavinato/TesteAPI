7bb20fd69e7e1c0a71044f17debaacf1d7b40895
guipavinato <guilherme.pavinato@gmail.com>
2023-01-10 21:39:19 -0300

    criado o projeto da api com o template, e criados os modelos das entidades

---
+++ TesteBackEnd/Models/TelefoneModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteBackEnd.Models
+{
+    public class TelefoneModel
+    {
+        public string? Numero { get; set; }
+        public int? UsuarioId { get; set; }
+        public virtual UsuarioModel? Usuario { get; set; }
+    }
+}
---
+++ TesteBackEnd/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TesteBackEnd.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateOnly Nascimento { get; set; }
+        public char? Cpf { get; set; }
+        public ICollection<TelefoneModel> Telefones { get; set; }
+    }
+}
