28cccf3af2f6452e7b25ac01c86c62d8908e770e
guipavinato <guilherme.pavinato@gmail.com>
2023-01-10 23:21:07 -0300

    cria repositorio de usuario e adiciona string de conexão nas propriedades

---
+++ TesteBackEnd/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteBackEnd.Data;
+using TesteBackEnd.Models;
+using TesteBackEnd.Repositorios.Interfaces;
+
+namespace TesteBackEnd.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly ApiDBContext _dbContext;
+
+        public UsuarioRepositorio(ApiDBContext apiDBContext)
+        {
+            _dbContext = apiDBContext;
+        }
+
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContext.Usuarios.ToListAsync();
+        }
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbContext.Usuarios.AddAsync(usuario);
+            await _dbContext.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado.");
+            }
+
+            usuarioPorId.Nome = usuario.Nome;
+            usuarioPorId.Nascimento = usuario.Nascimento;
+            usuarioPorId.Cpf = usuario.Cpf;
+            usuarioPorId.Telefones = usuario.Telefones;
+
+            _dbContext.Usuarios.Update(usuarioPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado.");
+            }
+
+            _dbContext.Usuarios.Remove(usuarioPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+
+
+
+    }
+}
